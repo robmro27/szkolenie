f9e5fcbda14cc8e3437f6a5d06aa7e75632af13f
robmro27 <robert.mroz1985@gmail.com>
2017-04-06 12:39:59 +0200

    task entity form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class DefaultController extends Controller
 {
     /**
@@ -13,11 +16,30 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        // just setup a fresh $task object (remove the dummy data)
+    $task = new \AppBundle\Entity\Task();
+
+    $form = $this->createFormBuilder($task)
+        ->add('name', TextType::class)
+        ->add('save', SubmitType::class, array('label' => 'Create Task'))
+        ->getForm();
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        // $form->getData() holds the submitted values
+        // but, the original `$task` variable has also been updated
+        $task = $form->getData();
+
         
+    }
+
+
         $user = $this->getUser();
         
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
+            'form' => $form->createView(),
             'user' => $user,
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
